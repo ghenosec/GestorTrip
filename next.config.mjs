8ae8973cfd6c345e3816636d87d2ccedc39f7e27
Ghenosec <lucasgheno600@gmail.com>
2026-02-24 22:55:12 -0300

    config project for app export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  assetPrefix: "./",
 }
 
 export default nextConfig
